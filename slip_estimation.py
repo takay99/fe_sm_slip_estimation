12ce642ffe5d35267eff5c12d266c32caca90b1a
takay99 <mclarenhonda.rc@gmail.com>
2026-02-14 17:21:29 +0900

    0214

--- slip_estimation.py
+++ slip_estimation.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     print("Hello from fe-sm-data-analize!")
 
     output_data = pd.read_csv(
-        "LOG00295.txt",
+        "sim.txt",
         header=None,
         delim_whitespace=False,
         # 古い引数の代わりに新しい引数を使う
@@ -139,3 +139,5 @@ if __name__ == "__main__":
     plt.show()
 
         # print(f"steer: {str}, steer_dot: {str_dot}")
+
+    
